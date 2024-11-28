fa91302848506cfea430d6541a87639f07a2666e
mathisboche <bibi_76@outlook.fr>
2024-11-28 01:58:21 +0100

    config de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // fichiers HTML où tailwind doit scanner les classes
+  theme: {
+    extend: {}, // espace pour personnaliser les couleurs, typographies, etc.
+  },
+  plugins: [], // aucun plugin par défaut
+};
