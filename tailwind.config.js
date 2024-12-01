c4af0a1235b165a235eb559f8ce80caa658710c4
mathisboche <bibi_76@outlook.fr>
2024-12-01 23:17:58 +0100

    bon, vsy j'ai tout delete mais au calme mdr, attend un peu de voir ce que ca va devenir

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.html"], // fichiers HTML où tailwind doit scanner les classes
-  theme: {
-    extend: {}, // espace pour personnaliser les couleurs, typographies, etc.
-  },
-  plugins: [], // aucun plugin par défaut
-};
